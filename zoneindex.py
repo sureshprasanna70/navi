c534464f0656415921323197399c7c1273df29ee
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-03-26 11:13:59 +0530

    Comments removed

--- zoneindex.py
+++ zoneindex.py
@@ -10,7 +10,6 @@ def main():
     while(tot_cars>0):
         all_tables.append(build_neighbor(dup_cars))
         tot_cars=tot_cars-1
-    #all_tables=[[-1, 2, 3], [1, -1, 3]]
     print all_tables
     zone_index=build_zone_index(all_tables,coverage_area)
     print 'Car {0} is the zone index'.format(zone_index)
